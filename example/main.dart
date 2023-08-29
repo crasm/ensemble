2c26818996b0d43539270b7b9da2d32433438447
crasm <crasm@git.vczf.us>
2023-08-29 03:35:30 -0400

    Maybe get callback working

--- example/main.dart
+++ example/main.dart
@@ -1,14 +1,17 @@
-import 'package:ensemble_llama/ensemble_llama.dart' as llama;
-import 'package:ensemble_llama/src/ensemble_llama_base.dart' as base;
+import 'package:ensemble_llama/ensemble_llama.dart';
+import 'package:ensemble_llama/src/ensemble_llama_base.dart';
 
 import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 
-void mycb(Float progress, Pointer<Void> ctx) {
+void mycb(double progress, Pointer<Void> ctx) {
   print(progress);
 }
 
 void main() {
-  var params = llama.contextDefaultParams();
+  var params = contextDefaultParams();
   params.progress_callback = Pointer.fromFunction(mycb);
+  print(params.progress_callback);
+  Pointer<NativeFunction<LlamaProgressCallback>> foo = params.progress_callback;
+  print(foo);
 }
