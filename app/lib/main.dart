095043ac407d408845cd58df3df24e523ea0ecfb
crasm <crasm@git.vczf.us>
2023-11-27 02:14:10 -0500

    app: get some basic things working
    * text has consistent styling while generating and when editable
    * bottom sheet sorta works

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -9,34 +9,43 @@ import 'package:ensemble_common/common.dart';
 import 'package:grpc/grpc.dart';
 
 void main() {
-  runApp(const App());
+  runApp(const EnsembleApp());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
+class EnsembleApp extends StatelessWidget {
+  const EnsembleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Ensemble',
+      theme: ThemeData(
+          useMaterial3: true,
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: Colors.lightBlue, brightness: Brightness.light)),
       home: Home(),
     );
   }
 }
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({super.key});
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Ensemble'),
-        backgroundColor: Colors.amber[300],
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (ctx) {
+              return [
+                PopupMenuItem(child: Text('Settings')),
+              ];
+            },
+          )
+        ],
       ),
       body: PageView(children: [
         GenPage(),
@@ -61,6 +70,10 @@ class _GenPageState extends State<GenPage> with AutomaticKeepAliveClientMixin {
 
   final String prompt = "Hello, my name is";
   late final StringBuffer gen;
+  final TextEditingController _controller = TextEditingController();
+  final ScrollController _genScrollController = ScrollController();
+
+  bool _isGenerating = false;
 
   @override
   void initState() {
@@ -76,36 +89,76 @@ class _GenPageState extends State<GenPage> with AutomaticKeepAliveClientMixin {
     );
 
     gen = StringBuffer(prompt);
-
-    stub.generate(Prompt(text: prompt)).listen(_loadGen);
+    stub.generate(Prompt(text: prompt)).listen(
+          _loadGen,
+          onDone: _onDoneGen,
+          onError: _onErrorGen,
+          cancelOnError: true,
+        );
+    _isGenerating = true;
   }
 
   void _loadGen(Token tok) {
     if (tok.hasText()) {
       setState(() {
         gen.write(tok.text);
+        _controller.text = gen.toString();
       });
     }
   }
 
+  void _onDoneGen() => _isGenerating = false;
+  void _onErrorGen(e) => _isGenerating = false;
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    return Stack(children: [
-      Text(gen.toString()),
-      DraggableScrollableSheet(
-        builder: (ctx, ctl) => Container(
-          color: Colors.lightGreen[100],
-          child: ListView(
-            controller: ctl,
-            children: [
-              ListTile(title: Text("temp")),
-              ListTile(title: Text("top K")),
-              ListTile(title: Text("top P")),
-            ],
-          ),
+    final genTextStyle = TextStyle(
+      fontFamily: 'monospace',
+      inherit: false,
+      color: Colors.black,
+      fontWeight: FontWeight.normal,
+      wordSpacing: 0.8,
+      letterSpacing: 0.0,
+      fontSize: 14.0,
+      height: 1.3,
+      leadingDistribution: null,
+      textBaseline: TextBaseline.alphabetic,
+    );
+
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: _isGenerating
+            ? SingleChildScrollView(child: Text(gen.toString(), style: genTextStyle))
+            : TextField(
+                controller: _controller,
+                scrollController: _genScrollController,
+                style: genTextStyle,
+                maxLines: null,
+                decoration: null,
+              ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() => _isGenerating = !_isGenerating),
+        child:
+            Icon(!_isGenerating ? Icons.play_circle_filled_sharp : Icons.pause_circle_filled_sharp),
+      ),
+      bottomSheet: DraggableScrollableSheet(
+        maxChildSize: 0.70,
+        initialChildSize: 0.15,
+        minChildSize: 0.1,
+        expand: false,
+        snap: true,
+        shouldCloseOnMinExtent: true,
+        builder: (context, scrollController) => ListView(
+          controller: scrollController,
+          children: ['Temperature', 'TopK', 'TopP', 'MinP']
+              .map((p) => FilledButton.tonal(onPressed: () {}, child: Text(p)))
+              .toList(),
         ),
       ),
-    ]);
+    );
   }
 }
