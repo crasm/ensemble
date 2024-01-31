20bad603f3d544db0418f112243f227c4f2ef715
crasm <crasm@git.vczf.net>
2024-01-31 00:10:09 -0500

    app: continue work on sampler UI

--- app/lib/samplers.dart
+++ app/lib/samplers.dart
@@ -5,20 +5,16 @@ import 'package:ensemble_protos/llamacpp.dart' as pb;
 sealed class Sampler {
   String get name;
   pb.Sampler get pbSampler;
-  Widget buildListChild(BuildContext context);
-  Dialog buildDialog(BuildContext context);
-}
 
-abstract class SamplerWithSingleValue<T> extends Sampler {
-  T value;
-  SamplerWithSingleValue(this.value);
-
-  Widget buildListChild(BuildContext context) {
+  Widget _buildListChild(
+    BuildContext context, {
+    required Widget child,
+  }) {
     return Card(
       key: Key(name),
       child: ListTile(
         title: Text(name),
-        trailing: Container(child: Text(value.toString())),
+        trailing: Container(child: child),
         onTap: () => showDialog(
           context: context,
           builder: buildDialog,
@@ -27,25 +23,71 @@ abstract class SamplerWithSingleValue<T> extends Sampler {
     );
   }
 
-  Dialog buildDialog(BuildContext context) {
+  Dialog _buildDialog(
+    BuildContext context, {
+    required List<Widget> children,
+  }) {
     return Dialog(
       child: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Text(name),
-        ]),
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [Text(name), ...children]),
       ),
     );
   }
+
+  Widget buildListChild(BuildContext context);
+  Dialog buildDialog(BuildContext context);
 }
 
-final class SamplerLogitBias extends SamplerWithSingleValue<Map<int, double>> {
-  SamplerLogitBias(Map<int, double> bias) : super(bias);
+abstract class SamplerWithSingleNumValue<T extends num> extends Sampler {
+  T value;
+  late final TextEditingController _valueController;
+  final T Function(String) parser;
+  SamplerWithSingleNumValue(this.value, this.parser) {
+    _valueController = TextEditingController(text: value.toString());
+  }
+
+  void dispose() => _valueController.dispose();
+
+  Widget buildListChild(BuildContext context) =>
+      _buildListChild(context, child: Text(value.toString()));
+
+  Dialog buildDialog(BuildContext context) {
+    return _buildDialog(context, children: [
+      TextField(
+        controller: _valueController,
+        keyboardType: TextInputType.number,
+        onSubmitted: (text) {
+          value = parser(text);
+        },
+      ),
+    ]);
+  }
+}
+
+final class SamplerLogitBias extends Sampler {
+  final Map<int, double> bias;
+  SamplerLogitBias(this.bias);
+
   @override
   String get name => 'Logit Bias';
 
   @override
-  pb.Sampler get pbSampler => pb.Sampler(logitBias: pb.LogitBias(bias: value));
+  pb.Sampler get pbSampler => pb.Sampler(logitBias: pb.LogitBias(bias: bias));
+
+  @override
+  Widget buildListChild(BuildContext context) {
+    return _buildListChild(context, child: Text(bias.toString()));
+  }
+
+  @override
+  Dialog buildDialog(BuildContext context) {
+    return _buildDialog(context, children: [
+      Text('unimplemented'),
+    ]);
+  }
 }
 
 final class SamplerRepetitionPenalty extends Sampler {
@@ -77,20 +119,13 @@ final class SamplerRepetitionPenalty extends Sampler {
 
   @override
   Widget buildListChild(BuildContext context) {
-    return Card(
-      key: Key(name),
-      child: ListTile(
-        title: Text(name),
-        trailing: Container(
-          child: Text(
-            'lastN=$lastN, penalty=$penalty\n'
-            'presencePenalty=$presencePenalty\n'
-            'frequencyPenalty=$frequencyPenalty',
-          ),
-        ),
-        onTap: () => showDialog(
-          context: context,
-          builder: buildDialog,
+    return _buildListChild(
+      context,
+      child: Container(
+        child: Text(
+          'lastN=$lastN, penalty=$penalty\n'
+          'presencePenalty=$presencePenalty\n'
+          'frequencyPenalty=$frequencyPenalty',
         ),
       ),
     );
@@ -98,19 +133,14 @@ final class SamplerRepetitionPenalty extends Sampler {
 
   @override
   Dialog buildDialog(BuildContext context) {
-    return Dialog(
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Text(name),
-        ]),
-      ),
-    );
+    return _buildDialog(context, children: [
+      Text('unimplemented'),
+    ]);
   }
 }
 
-class SamplerMinP extends SamplerWithSingleValue<double> {
-  SamplerMinP(double minP) : super(minP);
+class SamplerMinP extends SamplerWithSingleNumValue<double> {
+  SamplerMinP(double minP) : super(minP, double.parse);
 
   @override
   String get name => 'Min P';
@@ -119,8 +149,8 @@ class SamplerMinP extends SamplerWithSingleValue<double> {
   pb.Sampler get pbSampler => pb.Sampler(minP: pb.MinP(minP: value));
 }
 
-class SamplerTemperature extends SamplerWithSingleValue<double> {
-  SamplerTemperature(double temp) : super(temp);
+class SamplerTemperature extends SamplerWithSingleNumValue<double> {
+  SamplerTemperature(double temp) : super(temp, double.parse);
 
   @override
   String get name => 'Temperature';
