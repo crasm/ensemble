45296890b922210ecd82da179eebd6e9f246afa4
crasm <crasm@git.vczf.net>
2024-02-03 03:48:42 -0500

    reif: get basic writing working maybe

--- reif/example/main.dart
+++ reif/example/main.dart
@@ -1,69 +1,44 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'dart:io';
-import 'dart:typed_data';
 
-import 'package:ffi/ffi.dart';
-import 'package:reif/src/gen/text_append.pb.dart';
+import 'package:logging/logging.dart';
 import 'package:reif/src/reif_base.dart';
 
 // Reif future requirements
 // * Isolate for doing background IO and maintenance tasks.
 // * Cache deltas in memory and commit them in background
 
-class TextAppendReif {
-  static Future<TextAppendReif> fromFile(String filePath) async {
-    final file = await File(filePath).open(mode: FileMode.append);
+final class TextAppendReif extends Reif {
+  TextAppendReif(super.filePath);
 
-    // If file is more-or-less empty, initialize it
-    if (await file.length() < sizeOf<ReifHeader>()) {
-      final header = calloc.allocate(sizeOf<ReifHeader>()).cast<ReifHeader>();
-      final magic = ascii.encode('REIF');
-      for (var i = 0; i < magic.length; i++) {
-        header.ref.magicNumber[i] = magic[i];
-      }
-      header.ref.version = 1;
-
-      final buf = Uint8List.view(
-        header
-            .cast<Uint8>()
-            .asTypedList(
-              sizeOf<ReifHeader>(),
-            )
-            .buffer,
-      );
-
-      await file.writeFrom(buf);
-      calloc.free(header);
+  final _isEmpty = false;
+  Future<void> append(String value) async {
+    if (_isEmpty) {
+      await addCheckpoint(utf8.encode(value));
+    } else {
+      await addDelta(utf8.encode(value));
     }
-
-    return TextAppendReif(file);
   }
-
-  final RandomAccessFile _file;
-  final List<String> _deltas = [];
-
-  TextAppendReif(this._file) {}
-
-  Future<void> close() async {
-    await _file.close();
-  }
-
-  String get text {
-    final buf = StringBuffer()..writeAll(_deltas);
-    return buf.toString();
-  }
-
-  // set text(String value) => _text = value;
-  void append(String value) => _deltas.add(value);
 }
 
 void main() async {
-  final reif = await TextAppendReif.fromFile('./text-append.reif');
-  ['My name', ' is Bob.', ' What is your ', 'name? '].forEach((element) {
-    reif.append(element);
-    print(reif.text);
+  final startTime = DateTime.now();
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((e) {
+    final diff = e.time.difference(startTime);
+    stderr.writeln(
+      '${e.level.name.padRight(7)}: '
+      '${diff.inMilliseconds.toString().padLeft(6, '0')}: '
+      '${e.loggerName}: '
+      '${e.message}',
+    );
   });
 
-  await reif.close();
+  final reif = TextAppendReif('./text-append.reif');
+  for (final str in ['My name', ' is Bob.', ' What is your ', 'name? ']) {
+    await reif.append(str);
+    // print(reif.text);
+  }
+
+  await reif.dispose();
 }
--- reif/lib/src/reif_base.dart
+++ reif/lib/src/reif_base.dart
@@ -1,7 +1,135 @@
 import 'dart:ffi';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:ffi/ffi.dart';
+import 'package:logging/logging.dart';
+import 'package:meta/meta.dart';
+
+const _magicNumber = [0x52, 0x45, 0x49, 0x46];
+
+abstract class Reif {
+  static final _log = Logger('Reif');
+
+  final String file;
+  late final RandomAccessFile _file;
+  late final Pointer<ReifHeader> _headerPointer;
+  late final Pointer<EntryHeader> _entryPointer;
+  late final ReifHeader _header;
+  late final EntryHeader _entry;
+
+  /// Create a Reif, initializing the file if it does not exist.
+  Reif(this.file) {
+    _headerPointer = malloc.allocate(ReifHeader.size).cast<ReifHeader>()
+      ..zero();
+    _entryPointer = malloc.allocate(EntryHeader.size).cast<EntryHeader>()
+      ..zero();
+    _header = _headerPointer.ref;
+    _entry = _entryPointer.ref;
+
+    _file = File(file).openSync(mode: FileMode.append);
+    final fileLength = _file.lengthSync();
+    if (fileLength == 0) {
+      // Initialize file with Reif header
+      for (var i = 0; i < _magicNumber.length; i++) {
+        _header.magicNumber[i] = _magicNumber[i];
+      }
+      _header.version = 1;
+      _header.nextEntryOffset = ReifHeader.size;
+      _file.writeFromSync(_headerPointer.asBytes);
+    }
+
+    _file.setPositionSync(0);
+    var bytesRead = _file.readIntoSync(_headerPointer.asBytes);
+    if (bytesRead != ReifHeader.size) {
+      throw Exception(
+          "Couldn't read entire header from $file, only read $bytesRead bytes");
+    }
+
+    for (var i = 0; i < _magicNumber.length; i++) {
+      if (_header.magicNumber[i] != _magicNumber[i]) {
+        throw Exception("File at $file doesn't contain the REIF magic number");
+      }
+    }
+  }
+
+  @mustCallSuper
+  Future<void> dispose() async {
+    // Save the header
+    _file.setPositionSync(0);
+    _file.writeFromSync(_headerPointer.asBytes);
+    malloc.free(_headerPointer);
+    await _file.close();
+
+    malloc.free(_entryPointer);
+  }
+
+  Future<void> addCheckpoint(Uint8List bytes) =>
+      _addEntry(bytes, EntryType.checkpoint);
+  Future<void> addDelta(Uint8List bytes) => _addEntry(bytes, EntryType.delta);
+
+  Future<void> _addEntry(Uint8List bytes, int entryType) async {
+    switch (entryType) {
+      case EntryType.checkpoint:
+        _header.lastCheckpointOffset = _header.nextEntryOffset;
+      case EntryType.delta:
+        _header.lastDeltaOffset = _header.nextEntryOffset;
+      default:
+        assert(false, '_entryType $entryType is not a valid EntryType');
+    }
+
+    _file.setPositionSync(_header.nextEntryOffset);
+
+    _entryPointer.zero();
+    _entry.type = entryType;
+    _entry.payloadSize = bytes.length;
+
+    final remainder = _entry.payloadSize % 4;
+    _entry.payloadPadding = remainder > 0 ? 4 - remainder : 0;
+
+    _header.nextEntryOffset = _header.nextEntryOffset +
+        EntryHeader.size +
+        _entry.payloadSize +
+        _entry.payloadPadding;
+
+    _file.writeFromSync(_entryPointer.asBytes);
+    await _file.writeFrom(bytes);
+    for (var i = 0; i < _entry.payloadPadding; i++) {
+      await _file.writeByte(0);
+    }
+    // TODO(crasm): sync?
+    // await _file.flush();
+  }
+}
+
+extension on Pointer<ReifHeader> {
+  Uint8List get asBytes =>
+      Uint8List.view(cast<Uint8>().asTypedList(ReifHeader.size).buffer);
+
+  void zero() {
+    final bp = cast<Uint8>();
+    for (var i = 0; i < ReifHeader.size; i++) {
+      bp[0] = i;
+    }
+  }
+}
+
+extension on Pointer<EntryHeader> {
+  Uint8List get asBytes =>
+      Uint8List.view(cast<Uint8>().asTypedList(EntryHeader.size).buffer);
+
+  void zero() {
+    final bp = cast<Uint8>();
+    for (var i = 0; i < EntryHeader.size; i++) {
+      bp[0] = i;
+    }
+  }
+}
 
 // We also assume little-endian
 final class ReifHeader extends Struct {
+  static int get size => sizeOf<ReifHeader>();
+
   @Array(4)
   external Array<Uint8> magicNumber; // should be 'REIF' in ASCII;
 
@@ -14,7 +142,12 @@ final class ReifHeader extends Struct {
   @Uint32()
   external int lastDeltaOffset;
 
-  @Array(12)
+  // The offset to begin writing the EntryHeader for the next Entry. This is
+  // typically the length of the file.
+  @Uint32()
+  external int nextEntryOffset;
+
+  @Array(11)
   external Array<Uint32> reserved;
 }
 
@@ -26,14 +159,16 @@ abstract final class EntryType {
 // Payload is aligned/padded for 4-bytes alignment
 // TODO: this might be unnecessary but was fun to think about
 final class EntryHeader extends Struct {
+  static int get size => sizeOf<EntryHeader>();
+
   @Uint8()
   external int type;
 
   @Uint8()
-  external int prefixPadding; // Padding in bytes before payload begins
+  external int payloadPadding; // Padding in bytes after payload ends
 
-  @Uint8()
-  external int suffixPadding; // Padding in bytes after payload ends
+  @Uint16()
+  external int reserved;
 
   @Uint32()
   external int payloadSize;
